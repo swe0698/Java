b39c79dcbfdd4bba254469067a4ee2cabe26006a
swe0698 <swetha06111998@gmail.com>
2021-04-01 11:22:02 +0530

    First commit

---
+++ swe/src/Example.java
@@ -0,0 +1,11 @@
+
+public class Example {
+    public static void main(String[] args){
+
+     Integer average[] = new Integer[6];
+     for (int i =0; i<6;i++)
+     System.out.println(average[i]);
+
+
+    }
+}
